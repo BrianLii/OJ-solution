2806ad6594ebd0e4e898cec2ac2e04691a5f18a8
Brian Li <brianlee0415@gmail.com>
2025-01-10 18:03:39 +0800

    feat: update and verify UVA136.cpp

---
+++ UVA_verified/UVA136.cpp
@@ -0,0 +1,13 @@
+/*
+    Solution for: UVA 136 - Ugly Numbers
+    Problem Link: https://onlinejudge.org/external/1/136.pdf
+    Verdict: Accepted
+    Submission ID: 30091833
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    printf("The 1500\'th ugly number is 859963392.\n");
+    return 0;
+}
