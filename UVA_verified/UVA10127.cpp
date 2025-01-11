1b51caa1909c977b691433b8041096e77e773f4f
Brian Li <brianlee0415@gmail.com>
2025-01-11 13:49:43 +0800

    refactor: refactor UVA10127

--- UVA_verified/UVA10127.cpp
+++ UVA_verified/UVA10127.cpp
@@ -2,7 +2,7 @@
     Solution for: 10127 - Ones
     Problem Link: https://onlinejudge.org/external/101/10127.pdf
     Verdict: Accepted
-    Submission ID: 30092247
+    Submission ID: 30093369
 */
 
 #include <bits/stdc++.h>
@@ -11,12 +11,12 @@ using namespace std;
 class Solution {
    public:
     void solve() {
-        int x;
-        while (cin >> x) {
-            int curr = 1;
-            for (int i = 1, curr_sum = 1;;
-                 i++, curr_sum = (curr_sum * 10 + 1) % x) {
-                if (curr_sum == 0) {
+        int input;
+        while (cin >> input) {
+            int current_sum = 0;
+            for (int i = 1;; i++) {
+                current_sum = (current_sum * 10 + 1) % input;
+                if (current_sum == 0) {
                     cout << i << "\n";
                     break;
                 }
