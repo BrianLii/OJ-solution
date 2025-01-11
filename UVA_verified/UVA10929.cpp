a11068a1dffdffaa04ce2f62849486ba2613033d
Brian Li <brianlee0415@gmail.com>
2025-01-11 10:10:13 +0800

    lint: format UVA10929

--- UVA_verified/UVA10929.cpp
+++ UVA_verified/UVA10929.cpp
@@ -15,8 +15,10 @@ class Solution {
         while (cin >> N && N != "0") {
             int digit_sum = 0;
             for (int i = 0; i < N.size(); i++) {
-                if (i & 1) digit_sum += N[i] - '0';
-                else digit_sum -= N[i] - '0';
+                if (i & 1)
+                    digit_sum += N[i] - '0';
+                else
+                    digit_sum -= N[i] - '0';
             }
             if (digit_sum % 11) {
                 cout << N << " is not a multiple of 11.\n";
