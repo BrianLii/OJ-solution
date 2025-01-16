41e324622d586aa1b020c8cdd3c94dc5944f538f
Brian Li <brianlee0415@gmail.com>
2025-01-16 18:10:03 +0800

    feat: add UVA template

---
+++ UVA_verified/template.cpp
@@ -0,0 +1,21 @@
+/*
+    Solution for:
+    Problem Link:
+    Verdict: Accepted
+    Submission ID:
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+   public:
+    void solve() {}
+};
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    Solution().solve();
+    return 0;
+}
