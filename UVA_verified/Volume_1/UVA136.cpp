c82f84eae0004318a032f7944233a45b031dbb1e
Brian Li <brianlee0415@gmail.com>
2025-01-24 23:06:01 +0800

    feat: refactor UVA136

--- UVA_verified/Volume_1/UVA136.cpp
+++ UVA_verified/Volume_1/UVA136.cpp
@@ -2,12 +2,20 @@
     Solution for: UVA 136 - Ugly Numbers
     Problem Link: https://onlinejudge.org/external/1/136.pdf
     Verdict: Accepted
-    Submission ID: 30091833
+    Submission ID: 30114703
 */
 
 #include <bits/stdc++.h>
 using namespace std;
+
+class Solution {
+   public:
+    void solve() { cout << "The 1500\'th ugly number is 859963392." << endl; }
+};
+
 int main() {
-    printf("The 1500\'th ugly number is 859963392.\n");
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    Solution().solve();
     return 0;
 }
