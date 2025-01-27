37b67e492fa5191107547532f0cc8f5c058bf0b8
Brian Li <brianlee0415@gmail.com>
2025-01-27 15:57:05 +0800

    feat: implement UVA13025

---
+++ UVA/Volume_130/UVA13025.cpp
@@ -0,0 +1,21 @@
+/*
+    Solution for: UVA 13025 - Back to the Past
+    Problem Link: https://onlinejudge.org/external/130/13025.pdf
+    Verdict: Accepted
+    Submission ID: 30118093
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+   public:
+    void solve() { cout << "May 29, 2013 Wednesday\n"; }
+};
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    Solution().solve();
+    return 0;
+}
