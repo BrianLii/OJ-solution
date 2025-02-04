a6d3f570be467a804ec88dc0832170c89298f9fb
Brian Li <brianlee0415@gmail.com>
2025-02-04 18:06:22 +0800

    feat: improve UVA11462

--- UVA/Volume_114/UVA11462.cpp
+++ UVA/Volume_114/UVA11462.cpp
@@ -2,7 +2,8 @@
     Solution for: UVA 11462 - Age Sort
     Problem Link: https://onlinejudge.org/external/114/11462.pdf
     Verdict: Accepted
-    Submission ID: 30116921
+    Submission ID: 30130562
+    Tags: counting-sort
 */
 
 #include <bits/stdc++.h>
@@ -11,18 +12,25 @@ using namespace std;
 class Solution {
    public:
     void solve() {
-        int num_ages;
-        static int ages[2000000];
+        int num_ages, counts[100];
         while (cin >> num_ages && num_ages) {
-            for (int i = 0; i < num_ages; i++) {
-                cin >> ages[i];
+            fill(counts, counts + 100, 0);
+            for (int i = 0, age; i < num_ages; i++) {
+                cin >> age;
+                counts[age]++;
             }
-            sort(ages, ages + num_ages);
-            for (int i = 0; i < num_ages; i++) {
-                if (i) cout << ' ';
-                cout << ages[i];
+            bool is_first = true;
+            for (int i = 0; i < 100; i++) {
+                for (int j = 0; j < counts[i]; j++) {
+                    if (is_first) {
+                        is_first = false;
+                    } else {
+                        cout << ' ';
+                    }
+                    cout << i;
+                }
             }
-            cout << "\n";
+            cout << '\n';
         }
     }
 };
